ab7ab435f10ccd34fb32a50fc45b1ce1764aaaaa
mai.truong <mai.truong@student.passerellesnumeriques.org>
2019-05-10 19:34:41 +0700

    Controller

---
+++ app/Http/Controllers/ContryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contry;
+use Illuminate\Http\Request;
+
+class ContryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Contry  $contry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contry $contry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Contry  $contry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contry $contry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Contry  $contry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contry $contry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Contry  $contry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contry $contry)
+    {
+        //
+    }
+}
