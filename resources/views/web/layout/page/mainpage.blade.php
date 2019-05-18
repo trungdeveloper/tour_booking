87b081051d575e3182324564ca5c81eeec0070b8
hovantao <tao.ho@student.passerellesnumeriques.org>
2019-05-18 15:42:34 +0700

    layout web

---
+++ resources/views/web/layout/page/mainpage.blade.php
@@ -0,0 +1,361 @@
+@extends('layout.index')
+@section('content')
+<div class="container mainContainer">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mainContent">
+
+		<!-- BEGIN MOST INTERESTING HOTELS -->
+		<div class="col-xs-12 no-padding v-margin-top-30 v-margin-bottom-30">
+			<div class="col-xs-12 no-padding heading">
+
+				<h2 class="heading__title">Du lịch ngay! Vì bạn xứng đáng</h2>
+				<p class="heading__description">Chăm chỉ cả năm. Đến lúc tận hưởng</p>
+
+			</div>
+			<div class="col-xs-12 no-padding v-margin-bottom-15">
+				<div class="grid__container grid__container--topics super-cards topics owl-carousel  owl-carousel-mood owl-theme">
+
+					<div class="grid__item grid__item--1 item">
+						<div class="col-xs-3 super-card lazy " data-src="./Ivivu/Image/iVivu/2018/09/11/15/mood_1-4.png" data-was-processed="true" style="background-image: url('./Ivivu/Image/iVivu/2018/09/11/15/mood_1-4.png');">
+							<!--style="background-image: url('img');"-->
+							<a href="chu-de/khach-san-lang-man/index.htm" target="_blank">
+								<div class="col-xs-12 card-bg">
+									<div class="info">
+										<p class="title">L&#227;ng mạn</p>
+
+										<p class="description">Đi khắp thế gian, y&#234;u thương ngập tr&#224;n - <span style="text-decoration:underline;">31 khách sạn</span></p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="grid__item grid__item--2 item">
+						<div class="col-xs-3 super-card lazy " data-src="./Ivivu/Image/iVivu/2019/03/22/14/undefined.png" data-was-processed="true" style="background-image: url('./Ivivu/Image/iVivu/2019/03/22/14/undefined.png');">
+							<!--style="background-image: url('img');"-->
+							<a href="chu-de/khach-san-nghi-le/index.htm" target="_blank">
+								<div class="col-xs-12 card-bg">
+									<div class="info">
+										<p class="title">Nghỉ h&#232;</p>
+
+										<p class="description">Du lịch đi, chần chừ chi! - <span style="text-decoration:underline;">43 khách sạn</span></p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="grid__item grid__item--3 item">
+						<div class="col-xs-3 super-card lazy " data-src="./Ivivu/Image/iVivu/2018/09/11/15/mood_1-2.jpg" data-was-processed="true" style="background-image: url('./Ivivu/Image/iVivu/2018/09/11/15/mood_1-2.jpg');">
+							<!--style="background-image: url('img');"-->
+							<a href="chu-de/khach-san-dang-cap/index.htm" target="_blank">
+								<div class="col-xs-12 card-bg">
+									<div class="info">
+										<p class="title">Đẳng cấp</p>
+
+										<p class="description">Resort số một thế giới, nay đ&#227; trong tầm với - <span style="text-decoration:underline;">18 khách sạn</span></p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="grid__item grid__item--4 item">
+						<div class="col-xs-3 super-card lazy " data-src="./Ivivu/Image/iVivu/2018/09/11/15/mood_thu_gian.png" data-was-processed="true" style="background-image: url('./Ivivu/Image/iVivu/2018/09/11/15/mood_thu_gian.png');">
+							<!--style="background-image: url('img');"-->
+							<a href="chu-de/khach-san-tron-goi/index.htm" target="_blank">
+								<div class="col-xs-12 card-bg">
+									<div class="info">
+										<p class="title">Trọn g&#243;i</p>
+
+										<p class="description">Bỏ đi những muộn phiền, đặt combo đi liền - <span style="text-decoration:underline;">20 khách sạn</span></p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 no-padding v-margin-top-30 v-margin-bottom-30 hidden-xs">
+			<div class="col-xs-12 no-padding heading">
+				<h2 class="heading__title">Ưu đãi tốt nhất hôm nay</h2>
+				<p class="heading__description">Nhanh tay đặt ngay. Để mai sẽ lỡ</p>
+			</div>
+			<div class="col-xs-12 no-padding promotion-cards">
+				<div class="col-xs-12 no-padding owl-carousel owl-carousel-promotion owl-theme">
+					<div class="col-xs-12 promotion-card item">
+						<a href="khach-san-phan-thiet/khach-san-the-cliff-resort-residences-phan-thiet/index.htm" target="_blank">
+							<figure>
+								<picture>
+
+									<source media="(max-width: 768px)"  srcset="./Ivivu/Image/iVivu/2019/02/14/11/d-thecliff.png">
+										<source media="(max-width: 1024px)"  srcset="./Ivivu/Image/iVivu/2019/02/14/11/d-thecliff-1024x200.png" >
+											<source media="(max-width: 1200px)"  srcset="./Ivivu/Image/iVivu/2019/02/14/11/d-thecliff-1140x250.png" >
+												<img src="./Ivivu/Image/iVivu/2019/02/14/11/d-thecliff-1140x250.png">
+											</picture>
+										</figure>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 no-padding v-margin-top-30 v-margin-bottom-30">
+						<div class="col-xs-12 no-padding heading">
+							<h2 class="heading__title">Điểm đến yêu thích trong nước</h2>
+							<p class="heading__description">Lên rừng xuống biển. Trọn vẹn Việt Nam</p>
+						</div>
+						<!--<div class="headLineDivider"></div>-->
+						<div class="col-xs-12 no-padding">
+							<div class="grid__container grid__container--popular super-cards">
+
+								<div class="grid__item grid__item--1">
+									<div class="col-xs-12 super-card " style="background-image: url('./Ivivu/Image/iVivu/2019/03/07/16/phuquoc-755x190.jpg');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-phu-quoc/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Ph&#250; Quốc</p>
+
+													<p class="description hidden-xs"> 212 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--2">
+									<div class="col-xs-12 super-card card-auto-height" style="background-image: url('./Ivivu/Image/iVivu/2018/09/14/14/vungtau_2-370x395.jpg');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-vung-tau/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Vũng T&#224;u</p>
+
+													<p class="description hidden-xs"> 86 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--3">
+									<div class="col-xs-12 super-card " style="background-image: url('./Ivivu/Image/iVivu/2018/09/14/14/nhatrang_1-370x190.jpg');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-nha-trang/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Nha Trang</p>
+
+													<p class="description hidden-xs"> 179 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--4">
+									<div class="col-xs-12 super-card " style="background-image: url('./Ivivu/Image/iVivu/2018/09/14/14/quynhon_2-370x190.jpg');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-quy-nhon/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Quy Nhơn</p>
+
+													<p class="description hidden-xs"> 15 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--5">
+									<div class="col-xs-12 super-card " style="background-image: url('./Ivivu/Image/iVivu/2018/11/05/11/undefined-8-370x190.jpg');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-phan-thiet/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Phan Thiết</p>
+
+													<p class="description hidden-xs"> 93 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--6">
+									<div class="col-xs-12 super-card card-auto-height" style="background-image: url('./Ivivu/Image/iVivu/2018/09/14/14/danang_2-370x395.png');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-da-nang/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Đ&#224; Nẵng</p>
+
+													<p class="description hidden-xs"> 226 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--7">
+									<div class="col-xs-12 super-card " style="background-image: url('./Ivivu/Image/iVivu/2019/03/07/16/dalat-755x190.jpg');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-da-lat/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Đ&#224; Lạt</p>
+
+													<p class="description hidden-xs"> 132 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+								<div class="grid__item grid__item--8">
+									<div class="col-xs-12 super-card " style="background-image: url('./Ivivu/Image/iVivu/2019/03/07/16/sapaaaa-370x190.png');">
+										<!--style="background-image: url('img');"-->
+										<a href="khach-san-sapa/index.htm" target="_blank">
+											<div class="col-xs-12 card-bg">
+												<div class="info">
+													<p class="title">Sapa</p>
+
+													<p class="description hidden-xs"> 57 khách sạn</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+
+					<style>
+					.title-predict {
+						font-size: 14px;
+						font-weight: bold;
+						padding: 4px 8px;
+						background-color: #DDDDDD;
+					}
+					.item-place-predict {
+						font-size: 14px;
+						font-weight: bold;
+						padding: 4px 8px;
+					}
+				</style>
+				<!--BEGIN PREDICT GUEST-->
+
+
+
+				<!-- Modal from button Lấy giá tốt -->
+
+
+
+
+				<div class="modal fade findDealForCustomerModal" id="findDealForCustomerModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<h4 class="modal-title" style="padding-left:15px; text-transform: uppercase; ">
+									ĐẶT PHÒNG ĐOÀN
+									<i class="glyphicon glyphicon-info-sign fixGlyphicon vcolor-primary hidden-sm hidden-md hidden-lg" style="font-size:14px;"
+									data-toggle="tooltip" data-placement="bottom" title="iVIVU.com sẽ tìm kiếm những deal tốt nhất (khách sạn giá tốt nhất và tour giá tốt nhất) và
+									gửi đến địa chỉ mail của khách hàng dựa vào thông tin mà khách hàng cung cấp."></i>
+								</h4>
+							</div>
+							<div class="modal-body">
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 findDealForCustomerHeading vspacingbottom15">
+									<p class="hidden-xs">
+										Khi đặt phòng theo đoàn với iVIVU.com, bạn sẽ có được <b>khách sạn phù hợp nhất</b> với <b>giá tốt nhất</b> mà <b>không phải tốn thời gian tìm kiếm</b>.
+									</p>
+
+
+								</div>
+
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+									<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 vremoveSpacing">
+										<div class="form-group vspacingbottom25">
+											<label class="control-label">
+												<i class="glyphicon glyphicon-home hidden"></i> Yêu cầu
+											</label>
+											<div class="findDealForCustomerStyleCheckbox">
+												<div class="row">
+													<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+														<textarea id="typehotel" name="typehotel" class="form-control" rows="3" style="resize: vertical;" placeholder="Địa điểm, thời gian đặt phòng, số lượng thành viên của đoàn..."></textarea>
+													</div>
+												</div>
+											</div>
+
+
+										</div>
+
+										<div class="form-group vspacingbottom15">
+											<div class="row">
+												<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 vspacingbottom15">
+													<div style="position:relative;">
+														<label for="findDealForCustomerPhone" class="control-label"><i class="glyphicon glyphicon glyphicon-earphone hidden"></i> Điện thoại </label>
+
+														<div class="div-phone">
+															<input type="tel" class="form-control" id="findDealForCustomerPhone" name="phone" placeholder="Số điện thoại" onkeypress='return event.charCode >= 48 && event.charCode <= 57'>
+														</div>
+													</div>
+												</div>
+
+												<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+													<div class="div-email" style="position:relative;">
+														<label for="findDealForCustomerEmail" class="control-label">
+															<i class="glyphicon glyphicon-send hidden"></i> Email
+														</label>
+														<i class="vcolor-danger" style="font-size:28px;position: absolute;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Bắt buộc nhập">*</i>
+														<div>
+															<input type="text" class="form-control" id="findDealForCustomerEmail"
+															name="email" placeholder="Địa chỉ email" maxlength="100">
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<!--if have any error, set class has-error, if not, dont -->
+										<div class="alert alert-danger" role="alert" style="display:none">
+											<div class="pre-place" style="display:none">
+												<strong>
+													<i class="glyphicon glyphicon-warning-sign"></i>
+												</strong> Vui lòng nhập địa điểm.
+											</div>
+											<div class="pre-email">
+												<strong>
+													<i class="glyphicon glyphicon-warning-sign"></i>
+												</strong> Chưa nhập địa chỉ email hoặc địa chỉ email chưa đúng
+											</div>
+											<div class="pre-phone" style="display:none">
+												<strong>
+													<i class="glyphicon glyphicon-warning-sign"></i>
+												</strong> Vui lòng nhập số điện thoại.
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="modal-footer">
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 vspacingbottom10">
+									<div class="findDealForCustomerGuarantee hidden">
+										• iVIVU.com cam kết đảm bảo an toàn thông tin của quý khách hàng. <br>
+										• Quý khách hàng có thể yêu cầu ngưng dịch vụ tìm kiếm deal tốt nhất bất cứ lúc nào.
+									</div>
+
+									<button type="button" class="btn btn-warning btn-block btn-lg vbackground-warning" id="findDealForCustomerButton" onclick="ga('send', { 'hitType': 'event', 'eventCategory': 'Đăng ký tìm kiếm deal', 'eventAction': 'Click', 'eventLabel': 'Đăng ký tìm kiếm deal' });">
+										Đặt phòng đoàn
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<!--end modal from button lấy giá tốt-->
+				<!--END PREDICT GUEST-->
+
+			</div>
+		</div> <!-- Container -->
+
+@endsection
