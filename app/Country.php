057571116d3d628da80aa91f11fe849969aead5a
Modern Coding <cormorant.programming@gmail.com>
2019-05-19 22:37:13 +0700

    Country fixed

--- app/Country.php
+++ app/Country.php
@@ -6,23 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    protected $table='countries';
+    protected $table = 'countries'; // name of table in the database
     protected $guarded = ['id']; // fields in the table
     protected $fillable = ['label']; // fields in the table
 
     public $timestamps=true; // set timestamp, allow to use
-    
-    public $rules = [
-        'label' => 'required|unique:countries'
-      ];
 
     public $messages = [
         'label.required' => 'The name of the country is required',
         'label.unique' => 'This country name has already been taken'
       ];
 
-    public function user()
+
+    public function rules($id)
     {
-        return $this->hasMany('App\User');
+      return [
+        'label'   =>  'required|unique:countries,label,'.$id
+      ];
     }
 }
--- resources/views/country/_form.blade.php
+++ resources/views/country/_form.blade.php
@@ -17,6 +17,7 @@
 
   @if($country['id'] != NULL)
     @method('PATCH')
+    <input type="hidden" name="id" value="{!! $country['id'] !!}">
   @endif 
   
   <div class="row my-padding-bottom-19">
