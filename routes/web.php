d29d64d54889577371586a2e5d4f103120a1a378
trungdeveloper <tantrung12011999@gmail.com>
2019-05-20 09:43:45 +0700

    add table user_types

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resources([
+  'titles'                =>  'TitleController',   
+  'bookings'              =>  'BookingController',
+  'countries'             =>  'CountryController',
+  'destinations'          =>  'DestinationController',
+  'hotels'                =>  'HotelController',
+  'identificationTypes'   =>  'IdentificationTypeController',
+  'images'                =>  'ImageController',
+  'reviews'               =>  'ReviewController',
+  'services'              =>  'ServiceController',
+  'users'                 =>  'UserController',
+  'tours'                 =>  'TourController'
+]);
