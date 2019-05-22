455c959ea095f0525b155dbb5b870679aa993a11
Modern Coding <cormorant.programming@gmail.com>
2019-05-22 20:45:25 +0700

    UserRequest OK

--- database/migrations/2019_05_10_081248_create_users_table.php
+++ database/migrations/2019_05_10_081248_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('middle_name');
             $table->string('last_name');
-            $table->date('DoB');
+            $table->date('date_of_birth');
             $table->unsignedBigInteger('user_type_id');
             $table->unsignedBigInteger('country_id');
             $table->unsignedBigInteger('title_id');
