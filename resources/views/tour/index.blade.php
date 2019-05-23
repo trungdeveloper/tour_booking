ae9ccf31db21a915971c86a7be4910447379eaa6
mai.truong <mai.truong@student.passerellesnumeriques.org>
2019-05-23 19:20:13 +0700

    fix Tour

--- resources/views/tour/index.blade.php
+++ resources/views/tour/index.blade.php
@@ -4,61 +4,76 @@
 
   @include(
     '_layouts.indexHeader',
-    [
-      'title'         =>  "Tours",
-      'route'         =>  route('tours.create'),
-      'buttonLabel'   =>  "Add a new tour"
-    ]
+    ['title' => "Tours", 'route' => route('tours.create'), 'buttonLabel' => "Add a tour"]
   )
 
 @endsection
 
+
 @section('content')
-    <div class="row">
-      @foreach($tours as $tour)
 
-      <div class="col-md-6 col-lg-4 my-padding-bottom-19 my-filter-object my-tour">
-        <div class="my-frame">
-          <div class="my-padding-bottom-12 my-filter-target">
-            {!! $tour["name"] !!}
-          </div>
-          <div class="my-padding-bottom-12 my-filter-target">
-            {!! $tour["price"] !!}$
-          </div>
-          
-          <div class="d-flex flex-wrap">
+  @foreach($destinations as $destination)
 
-            <div class="my-padding-right-8 my-padding-bottom-8">
-              <a href="{!! route('tours.show', $tour['id']) !!}" class="btn btn-sm btn-outline-dark">
-                <i class="fas fa-eye my-margin-right-12"></i>
-                <span>Detail</span>
-              </a>
+    @if (count($destination->tours) > 0)
+
+      <div class="my-tour-type">
+
+        <h4 class="my-margin-bottom-19 my-margin-top-40 my-border-bottom">
+          <strong>
+            <em>{!! $destination->label !!}</em>
+          </strong>
+        </h4>
+
+
+        <div class="row">
+        
+          @foreach($destination->tours as $tour)
+
+            <div class="col-md-6 col-lg-4 my-padding-bottom-19 my-filter-object my-tour">
+              <div class="my-frame">
+                <div class="my-padding-bottom-12 my-filter-target">
+                  {!! $tour["title"]["label"].' '.$tour["name"].' '.$tour["price"].' '.$tour["desciption"] !!}
+                </div>
+                
+                <div class="d-flex flex-wrap">
+
+                  <div class="my-padding-right-8 my-padding-bottom-8">
+                    <a href="{!! route('tours.show', $tour["id"]) !!}" class="btn btn-sm btn-outline-dark">
+                      <i class="fas fa-eye my-margin-right-12"></i>
+                      <span>Detail</span>
+                    </a>
+                  </div>
+                  
+                  <div class="my-padding-right-8 my-padding-bottom-8">
+                    <a href="{!! route('tours.edit', $tour["id"]) !!}" class="btn btn-sm btn-outline-primary">
+                      <i class="far fa-edit my-margin-right-12"></i>
+                      <span>Edit</span>
+                    </a>
+                  </div>
+
+                  <div class="my-padding-bottom-8">
+                    <button
+                      class="btn btn-sm btn-danger my-tour-delete"
+                      data-token="{!! csrf_token() !!}"
+                      data-url="{!! route('tours.destroy', $tour['id']) !!}"
+                    >
+                      <i class="far fa-trash-alt my-margin-right-12"></i>
+                      <span>Delete</span>
+                    </button>
+                  </div>
+
+                </div>
+              </div>
             </div>
-            
-            <div class="my-padding-right-8 my-padding-bottom-8">
-              <a href="{!! route('tours.edit', $tour['id']) !!}" class="btn btn-sm btn-outline-primary">
-                <i class="far fa-edit my-margin-right-12"></i>
-                <span>Edit</span>
-              </a>
-            </div>
 
-            <div class="my-padding-bottom-8">
-              <button
-                class="btn btn-sm btn-danger my-tour-delete"
-                data-token="{!! csrf_token() !!}"
-                data-url="{!! route('tours.destroy', $tour['id']) !!}"
-              >
-                <i class="far fa-trash-alt my-margin-right-12"></i>
-                <span>Delete</span>
-              </button>
-            </div>
+          @endforeach
 
-          </div>
         </div>
+
       </div>
 
-    @endforeach
+    @endif
 
-  </div>
+  @endforeach
 
 @endsection
