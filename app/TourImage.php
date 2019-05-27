fc0b36bcf2c7c015652aedbba08f6957b445c76a
vinh.nguyen <vinh.mguyen@student.passerellesnumeriques.org>
2019-05-27 09:04:46 +0700

    create ajax-model-request-controller-route for tourImage

--- app/TourImage.php
+++ app/TourImage.php
@@ -8,7 +8,6 @@ class TourImage extends Model
 {
     protected $table='tour_images';
     protected $guarded = ['id']; // fields in the table
-   
     
     public $timestamps=true; // set timestamp, allow to use
     
