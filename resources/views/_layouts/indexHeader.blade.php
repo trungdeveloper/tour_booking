a4640d1a61aba53819540abf7b8db31eac2d54ed
Modern Coding <cormorant.programming@gmail.com>
2019-05-28 15:31:58 +0700

    Working on login => aside OK

--- resources/views/_layouts/indexHeader.blade.php
+++ resources/views/_layouts/indexHeader.blade.php
@@ -1,10 +1,14 @@
 @include('_layouts.header', ['title' => $title])
 
-<a href="{!! $route !!}" class="btn btn-sm btn-success">
-  <i class="fa fa-plus-circle my-margin-right-12"></i>
-  <span>{!! $buttonLabel !!}</span>
-</a>
+@if (Auth::check() && Auth::user()->hasAdminRights() || isset($forceDisplay) && $forceDisplay)
+  <a href="{!! $route !!}" class="btn btn-sm btn-success">
+    <i class="fa fa-plus-circle my-margin-right-12"></i>
+    <span>{!! $buttonLabel !!}</span>
+  </a>
+@endif
 
 <input class="form-control my-filter my-margin-bottom-19 my-margin-top-40" type="text" placeholder="Filter...">
 
-<div class="d-none my-margin-bottom-19" id="my-entity-delete-status"></div>
+@if (Auth::check() && Auth::user()->hasAdminRights())
+  <div class="d-none my-margin-bottom-19" id="my-entity-delete-status"></div>
+@endif
